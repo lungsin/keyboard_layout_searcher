6efa5fd19995de818f7b9a6edd9b2e95fe72d181
Kwee Lung Sin <kweelungsin@gmail.com>
2023-12-21 02:38:43 +0800

    Setup bazel, clangd, clang-format, and vscode extensions recommendation

---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+#include "layout.h"
+
+int main(int, char **) {
+  std::cout << "Hello, from klungsyzer!\n";
+
+  auto x = "unused var";
+
+  return 0;
+}
